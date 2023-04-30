0665aac5065adc84fc4fdad143f0cded88e0d8a5
borzx <1985borz@mail.ru>
2023-04-30 13:24:40 +0300

    tasks

---
+++ task2/task2.cs
@@ -0,0 +1,19 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+//
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string num_string = num.ToString();
+char[] num_array = num_string.ToCharArray();
+
+if (num > 99)
+{
+    Console.WriteLine($"Третья цифра: {num_array[2]}");
+}
+else
+Console.WriteLine($"Третьей цифры нет!");
---
+++ task3/task3.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введите число: ");
+int day_num = Convert.ToInt32(Console.ReadLine());
+
+
+if (day_num>=1 & day_num<=5)
+{
+        Console.WriteLine("Рабочий день");
+}
+else if (day_num==6 | day_num==7)
+{
+        Console.WriteLine("Выходной день");
+}
+else Console.WriteLine("Вы ввели неверное число!");
